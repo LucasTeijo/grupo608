dd9d5c21869bc2cfca29c7bd3ea18f53e9424374
GonzaloVodka <gonzalrodriguez@alumno.unlam.edu.ar>
2020-05-17 15:59:55 -0300

    Agrego ingreso al login y al registro (ambos vacios)

--- codigo/app/src/main/java/com/example/linterna/MainActivity.java
+++ codigo/app/src/main/java/com/example/linterna/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.linterna;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,30 +8,25 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginButton;
-    private Button registerButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.loginButton = findViewById(R.id.login_button);
-        this.registerButton = findViewById(R.id.register_button);
     }
 
     /**
      * This is called when the login button is pressed
      */
     public void goToLoginView(View view) {
-        System.out.println("Login");
+        startActivity(new Intent(this, LoginActivity.class));
     }
 
     /**
      * This is called when the register button is pressed
      */
     public void goToRegisterView(View view) {
-        System.out.println("Register");
+        startActivity(new Intent(this, RegisterActivity.class));
+
     }
 
 }
