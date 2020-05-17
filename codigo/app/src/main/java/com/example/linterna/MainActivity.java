757ca1baab64d2516e0386ed36846ab1db7d1c41
GonzaloVodka <gonzalrodriguez@alumno.unlam.edu.ar>
2020-05-17 15:56:18 -0300

    Agrego pantalla principal con botones

--- codigo/app/src/main/java/com/example/linterna/MainActivity.java
+++ codigo/app/src/main/java/com/example/linterna/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.linterna;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.loginButton = findViewById(R.id.login_button);
+        this.registerButton = findViewById(R.id.register_button);
     }
+
+    /**
+     * This is called when the login button is pressed
+     */
+    public void goToLoginView(View view) {
+        System.out.println("Login");
+    }
+
+    /**
+     * This is called when the register button is pressed
+     */
+    public void goToRegisterView(View view) {
+        System.out.println("Register");
+    }
+
 }
